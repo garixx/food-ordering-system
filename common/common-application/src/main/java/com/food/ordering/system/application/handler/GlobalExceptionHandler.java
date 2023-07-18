f9080401b2f738adc37edce8add6a7bed0547e97
akirraint@gmail.com <Nickel12345>
2023-07-18 18:08:20 +0300

    output ports added

---
+++ common/common-application/src/main/java/com/food/ordering/system/application/handler/GlobalExceptionHandler.java
@@ -0,0 +1,60 @@
+package com.food.ordering.system.application.handler;
+
+import com.food.ordering.system.application.ErrorDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import javax.validation.ValidationException;
+import java.util.stream.Collectors;
+
+@Slf4j
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ResponseBody
+    @ExceptionHandler(value = {Exception.class})
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorDTO handleException(Exception exception) {
+        log.error(exception.getMessage(), exception);
+        return ErrorDTO.builder()
+                .code(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase())
+                .message("Unexpected error!")
+                .build();
+    }
+
+    @ResponseBody
+    @ExceptionHandler(value = {ValidationException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorDTO handleException(ValidationException exception) {
+        ErrorDTO errorDTO;
+
+        if (exception instanceof ConstraintViolationException) {
+            String  violations = extractViolationsFromException((ConstraintViolationException) exception);
+            log.error(violations, exception);
+            errorDTO = ErrorDTO.builder()
+                    .code(HttpStatus.BAD_REQUEST.getReasonPhrase())
+                    .message(violations)
+                    .build();
+        } else {
+            log.error(exception.getMessage(), exception);
+            errorDTO = ErrorDTO.builder()
+                    .code(HttpStatus.BAD_REQUEST.getReasonPhrase())
+                    .message(exception.getMessage())
+                    .build();
+        }
+        return errorDTO;
+    }
+
+    private String extractViolationsFromException(ConstraintViolationException exception) {
+        return exception.getConstraintViolations()
+                .stream()
+                .map(ConstraintViolation::getMessage)
+                .collect(Collectors.joining("--"));
+    }
+}
---
+++ order-service/order-dataaccess/src/main/java/com/food/ordering/system/order/service/dataaccess/customer/repository/CustomerJpaRepository.java
@@ -0,0 +1,15 @@
+package com.food.ordering.system.order.service.dataaccess.customer.repository;
+
+import com.food.ordering.system.domain.valueobject.CustomerId;
+import com.food.ordering.system.order.service.dataaccess.customer.entity.CustomerEntity;
+import com.food.ordering.system.order.service.dataaccess.order.entity.OrderEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface CustomerJpaRepository extends JpaRepository<CustomerEntity, UUID> {
+    Optional<CustomerEntity> findById(CustomerId customerId);
+}
