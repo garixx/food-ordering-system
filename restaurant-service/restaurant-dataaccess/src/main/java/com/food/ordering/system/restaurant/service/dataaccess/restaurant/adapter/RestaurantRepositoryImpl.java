0bf41517fcafc59e43f6852921f7c285346d097a
akirraint@gmail.com <Nickel12345>
2023-07-29 18:19:06 +0300

    restaurant-dataccess module implemented and added

---
+++ restaurant-service/restaurant-dataaccess/src/main/java/com/food/ordering/system/restaurant/service/dataaccess/restaurant/adapter/RestaurantRepositoryImpl.java
@@ -0,0 +1,30 @@
+package com.food.ordering.system.restaurant.service.dataaccess.restaurant.adapter;
+
+import com.food.ordering.system.dataaccess.restaurant.repository.RestaurantJpaRepository;
+import com.food.ordering.system.restaurant.service.dataaccess.restaurant.mapper.RestaurantDataAccessMapper;
+import com.food.ordering.system.restaurant.service.domain.entity.Restaurant;
+import com.food.ordering.system.restaurant.service.domain.ports.output.repository.RestaurantRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class RestaurantRepositoryImpl implements RestaurantRepository {
+    private final RestaurantJpaRepository restaurantJpaRepository;
+    private final RestaurantDataAccessMapper restaurantDataAccessMapper;
+
+    public RestaurantRepositoryImpl(RestaurantJpaRepository restaurantJpaRepository,
+                                    RestaurantDataAccessMapper restaurantDataAccessMapper) {
+        this.restaurantJpaRepository = restaurantJpaRepository;
+        this.restaurantDataAccessMapper = restaurantDataAccessMapper;
+    }
+
+
+    @Override
+    public Optional<Restaurant> findRestaurantInformation(Restaurant restaurant) {
+        var restaurantProducts = restaurantDataAccessMapper.restaurantToRestaurantProducts(restaurant);
+        return restaurantJpaRepository
+                .findByRestaurantIdAndProductIdIn(restaurant.getId().getValue(), restaurantProducts)
+                .map(restaurantDataAccessMapper::restaurantEntityToRestaurant);
+    }
+}
